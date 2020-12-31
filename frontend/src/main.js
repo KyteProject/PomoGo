af1268db6cf8b45963f3030a01a19b93c51ca61e
KyteProject <headhertz88@gmail.com>
2020-12-31 12:41:34 +0000

    vuex empty store

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,14 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Vue from 'vue';
 import App from './App.vue';
+// import store from './store'
+import * as Wails from '@wailsapp/runtime';
+import VTooltip from 'v-tooltip'
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
 
-import * as Wails from '@wailsapp/runtime';
+Vue.use(VTooltip, { defaultDelay: 600, defaultOffset: 16 })
 
 Wails.Init(() => {
 	new Vue({
